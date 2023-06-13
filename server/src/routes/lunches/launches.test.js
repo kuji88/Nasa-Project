7ec726ac4f23e4d94bb23d1604306771e3523b8e
Kuji <kuji99@aol.com>
2023-06-13 14:44:42 +0300

    Testing API

---
+++ server/src/routes/lunches/launches.test.js
@@ -0,0 +1,16 @@
+describe("Test GET /Lanuches",()=>{
+    test('should be success', () => { 
+        const response = 200;
+        expect(response).toBe(200);
+     })
+})
+
+
+describe("Test POST /launches",()=>{
+    test("should post it",()=>{
+
+    })
+
+    test("should catch the missing", ()=>{})
+    test("should get the Date correctly", ()=>{})
+})
