0dbe91a205c79f5d5aa56d19dc3c50ca3f7b384d
Kuji <kuji99@aol.com>
2023-06-15 19:16:10 +0300

    Preparing to add MongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,20 @@
 const app = require('./app');
 const {loadPlanets} = require('./model/Planets.model')
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT;
+const MONGO_URL = 'mongodb+srv://kuji:LwrEhnFGNa6Zh5hM@nasa.pg4sgma.mongodb.net/?retryWrites=true&w=majority'
+
+    mongoose.connection.once('open',()=>{
+        console.log("mongo is ready!")
+    })
+
+    mongoose.connection.on('error',(err)=>{
+        console.error(err)
+    })
 
  const loadserver= async()=>{
+    await mongoose.connect(MONGO_URL)
     await loadPlanets();
    app.app.listen(PORT, ()=>{
         console.log(`this server is working on ${PORT}`)
